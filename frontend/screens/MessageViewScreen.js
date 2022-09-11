8541611d226dda3c3195cb317389d68ea018d962
r3dxm <moham3d.maaxin@gmail.com>
2022-09-11 22:16:43 +0530

    added more detail to message view screen, created a component for messages

--- frontend/screens/MessageViewScreen.js
+++ frontend/screens/MessageViewScreen.js
@@ -5,23 +5,42 @@ import {
 	TextInput,
 	TouchableOpacity,
 	Text,
+	ScrollView,
 } from "react-native";
 import Colors from "../libraries/Colors";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
+import Message from "../components/Message";
 
 function MessageViewScreen(props) {
+	const navigation = useNavigation();
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
-				<FontAwesome
-					name="arrow-left"
-					size={32}
-					color={Colors.white}
-					style={styles.arrow}
-				/>
-				<Text>Person Personson</Text>
+				<TouchableOpacity onPress={() => navigation.replace("MessageScreen")}>
+					<FontAwesome
+						name="arrow-left"
+						size={20}
+						color={Colors.white}
+						style={styles.arrow}
+					/>
+				</TouchableOpacity>
+				<Text style={styles.personName}>Person Personson</Text>
 			</View>
-			<View style={styles.messageHistory}></View>
+			<ScrollView style={styles.messageHistory}>
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+			</ScrollView>
 			<View style={styles.messageInput}>
 				<TextInput style={styles.inputBox} />
 				<TouchableOpacity style={styles.sendButton}>
@@ -49,8 +68,9 @@ const styles = StyleSheet.create({
 	inputBox: {
 		backgroundColor: Colors.grey,
 		height: 50,
-		width: 250,
+		width: 300,
 		marginRight: 20,
+		paddingLeft: 20,
 		borderRadius: 10,
 	},
 	sendButton: {
@@ -66,14 +86,23 @@ const styles = StyleSheet.create({
 		color: Colors.white,
 	},
 	header: {
+		flexDirection: "row",
 		width: "100%",
-		height: 50,
-		justifyContent: "center",
+		height: 60,
+		justifyContent: "flex-start",
 		alignItems: "center",
 		backgroundColor: Colors.orange,
 	},
 	arrow: {
-		alignSelf: "flex-start",
+		marginLeft: 20,
+	},
+	personName: {
+		marginLeft: 20,
+		fontSize: 20,
+		color: Colors.white,
+	},
+	messageHistory: {
+		width: "100%",
 	},
 });
 
